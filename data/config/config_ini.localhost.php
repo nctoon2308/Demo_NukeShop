15bde02a05bbbda08340c406dca46be3d35ff92d
Ton <tontiti552@gmail.com>
2020-11-20 08:24:49 +0700

    20/11/2020

--- data/config/config_ini.localhost.php
+++ data/config/config_ini.localhost.php
@@ -5,7 +5,7 @@
  * @Author VINADES.,JSC <contact@vinades.vn>
  * @Copyright (C) 2020 VINADES.,JSC. All rights reserved
  * @License GNU/GPL version 2 or any later version
- * @Createdate Wed, 18 Nov 2020 02:52:14 GMT
+ * @Createdate Thu, 19 Nov 2020 03:56:49 GMT
  */
 
 if (!defined('NV_MAINFILE')) {
@@ -38,4 +38,4 @@ ini_set('session.gc_maxlifetime', '3600');
 ini_set('track_errors', '1');
 ini_set('user_agent', 'NV4');
 
-$iniSaveTime = 1605667934;
+$iniSaveTime = 1605758209;
