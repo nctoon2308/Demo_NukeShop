96437018768bf80a84fe3e75c820e6adb1113fb9
nctoon2308 <namda1512016@gmail.com>
2020-11-26 14:00:18 +0700

    hiển thị trạng thái đơn hàng trong mục xem đơn hàng

--- modules/category/admin/order.php
+++ modules/category/admin/order.php
@@ -61,6 +61,8 @@ $xtpl->assign('OP', $op);
 //-------------------------------
 // Viết code xuất ra site vào đây
 
+
+
 if (!empty($array_row)){
     $i = ($page-1) * $perpage;
     foreach ($array_row as $row){
@@ -70,6 +72,21 @@ if (!empty($array_row)){
 
         $row['url_delete'] = NV_BASE_ADMINURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE . '=' .$module_name. '&amp;' . NV_OP_VARIABLE .'=order&amp;id='.$row['id'].'&action=delete&checksess='. md5($row['id'].NV_CHECK_SESSION) ;
         $row['url_edit'] = NV_BASE_ADMINURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE . '=' .$module_name.'&amp;' . NV_OP_VARIABLE . '=crud_order&amp;id=' . $row['id'];
+
+        if (!empty($row['order_status'])){
+            if ($row['order_status'] == 1)
+                $row['order_status'] = 'Đang chờ xác nhận';
+            elseif ($row['order_status'] == 2)
+                $row['order_status'] = 'Đã xác nhận';
+            elseif ($row['order_status'] == 3)
+                $row['order_status'] = 'Đang vận chuyển';
+            elseif ($row['order_status'] == 4)
+                $row['order_status'] = 'Hoàn tất';
+            elseif ($row['order_status'] == 5)
+                $row['order_status'] = 'Đơn hủy';
+            else
+                $row['order_status'] = 'Đã hoàn tiền ( hủy đơn )';
+        }
 
         $xtpl->assign('ROW',$row);
         $xtpl->parse('main.loop');
