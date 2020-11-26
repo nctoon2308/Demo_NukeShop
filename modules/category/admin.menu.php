968f8afbc1ae80118b2b3f58743c909626f39407
nctoon2308 <namda1512016@gmail.com>
2020-11-26 10:33:03 +0700

    xoá menu thêm, sửa đơn hàng

--- modules/category/admin.menu.php
+++ modules/category/admin.menu.php
@@ -18,6 +18,5 @@ $submenu['crud_cate'] = $lang_module['crud_cate'];
 $submenu['list_product'] = $lang_module['list_product'];
 $submenu['crud_product'] = $lang_module['crud_product'];
 $submenu['order'] = $lang_module['order'];
-$submenu['crud_order'] = $lang_module['crud_order'];
 
 
